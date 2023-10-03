ad4e5d94194c5675fb88ad568317bceb9bc3bcdc
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-10-03 08:52:04 -0600

    Feat(pokemon): Style image and put it in a container

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,15 +1,17 @@
 <template>
-  <img
-    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-    class="hidden-pokemon"
-    alt="pokemon"
-  />
+  <div class="pokemon-container">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      class="hidden-pokemon"
+      alt="pokemon"
+    />
 
-  <img
-    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-    class="fade-in"
-    alt="pokemon"
-  />
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      class="fade-in"
+      alt="pokemon"
+    />
+  </div>
 </template>
 
 <script>
@@ -17,6 +19,20 @@ export default {};
 </script>
 
 <style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
 .hidden-pokemon {
   filter: brightness(0);
 }
