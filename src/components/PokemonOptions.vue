fe23b0e8ff3e8293e8cb23d8c780b58fb0a9aa39
alejandroq12 <jaquezada@innovacion.gob.sv>
2023-10-03 09:08:41 -0600

    Feat(options): Add options

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,13 +1,37 @@
 <template>
-    <h1>Pokemon options.</h1>
-  </template>
-  
-  <script>
-  export default {
-  
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+  <div class="options-container">
+    <ul>
+      <li>Option 1</li>
+      <li>Option 2</li>
+      <li>Option 3</li>
+      <li>Option 4</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+li {
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 250px;
+}
+
+li:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
