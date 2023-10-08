c6a0b5f7eb92edf024be967d71c8dd6f9920e7d6
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-10-07 21:10:24 -0600

    Feat(test): Test array elements.

--- tests/unit/helpers/getPokemonOptions.spec.js
+++ tests/unit/helpers/getPokemonOptions.spec.js
@@ -4,5 +4,8 @@ describe('getPokemonOptions helpers', () => {
   test('Should return an array of numbers', () => {
     const pokemons = getPokemons();
     expect(pokemons.length).toBe(650);
+    expect(pokemons[0]).toBe(1);
+    expect(pokemons[500]).toBe(501);
+    expect(pokemons[649]).toBe(650);
   });
 });
