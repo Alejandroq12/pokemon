8f6f1716c94b71228cd6505ec8e1671994d6243b
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-10-07 20:58:43 -0600

    Fix(import): Import the right function.

--- tests/unit/helpers/getPokemonOptions.spec.js
+++ tests/unit/helpers/getPokemonOptions.spec.js
@@ -1,4 +1,4 @@
-import getPokemonOptions from '@/helpers/getPokemonOptions'
+import { getPokemons } from '@/helpers/getPokemonOptions'
 
 describe('getPokemonOptions helpers', () => {
     test('Should return an array of numbers', () => {
